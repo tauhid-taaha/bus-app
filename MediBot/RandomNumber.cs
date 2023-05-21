1875371c064dd2dde8ec714331e08f11fa071c2d
Lenovo <Lenovo@DESKTOP-FM15DRD>
2023-05-21 12:10:07 +0600

    ok

--- MediBot/RandomNumber.cs
+++ MediBot/RandomNumber.cs
@@ -9,7 +9,7 @@ namespace MediBot
     public static class RandomNumber
     {
         static Random random = new Random();
-        static public int randomNumber = random.Next(1000, 9999);
+        static public int randomNumber = random.Next(1000, 9909);
         //generating random numbers
     }
 }
